ab51dff8db9b04ab8dff7cec914030d53eae43c5
zz <zz>
2023-12-13 17:10:51 +0800

    save

--- server.py
+++ server.py
@@ -244,9 +244,6 @@ class PromptServer():
                 if os.path.isfile(file):
                     with Image.open(file) as original_pil:
                         metadata = PngInfo()
-                        # encrypt
-                        encrypt_image_v2(original_pil, get_sha256('123'))
-                        metadata.add_text("encrypt", "1")
                         if hasattr(original_pil,'text'):
                             for key in original_pil.text:
                                 metadata.add_text(key, original_pil.text[key])
@@ -255,6 +252,9 @@ class PromptServer():
                         # alpha copy
                         new_alpha = mask_pil.getchannel('A')
                         original_pil.putalpha(new_alpha)
+                        # encrypt
+                        encrypt_image_v2(original_pil, get_sha256('123'))
+                        metadata.add_text("encrypt", "1")
                         original_pil.save(filepath, compress_level=4, pnginfo=metadata)
 
             return image_upload(post, image_save_function)
